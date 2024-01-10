2d5ef97a23bd7371fcdf09ec2ee00040467fe5e5
DAT-DO <datdonguyen99@gmail.com>
2024-01-10 11:15:03 +0700

    change handle exception

--- src/main/java/net/sparkminds/librarymanagement/service/impl/AuthServiceImpl.java
+++ src/main/java/net/sparkminds/librarymanagement/service/impl/AuthServiceImpl.java
@@ -3,13 +3,13 @@ package net.sparkminds.librarymanagement.service.impl;
 import lombok.RequiredArgsConstructor;
 import net.sparkminds.librarymanagement.entity.Role;
 import net.sparkminds.librarymanagement.entity.User;
-import net.sparkminds.librarymanagement.exception.LibraryManagementException;
+import net.sparkminds.librarymanagement.exception.UserAlreadyExistException;
+import net.sparkminds.librarymanagement.exception.UserNotFoundException;
 import net.sparkminds.librarymanagement.payload.RegisterDTO;
 import net.sparkminds.librarymanagement.repository.RoleRepository;
 import net.sparkminds.librarymanagement.repository.UserRepository;
 import net.sparkminds.librarymanagement.service.AuthService;
 import net.sparkminds.librarymanagement.utils.RoleName;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +30,7 @@ public class AuthServiceImpl implements AuthService {
         // check for email exists in db
         boolean existEmail = userRepository.existsByEmail(registerDTO.getEmail());
         if (existEmail) {
-            throw new LibraryManagementException(HttpStatus.BAD_REQUEST, "Email is already exist!");
+            throw new UserAlreadyExistException("user.email.email-existed");
         }
 
         // create user entity using Builder pattern
@@ -42,7 +42,7 @@ public class AuthServiceImpl implements AuthService {
 
         // set role is USER by default
         Role role = roleRepository.findByRoleName(RoleName.ROLE_USER)
-                .orElseThrow(() -> new LibraryManagementException(HttpStatus.BAD_REQUEST, "Role not found"));
+                .orElseThrow(() -> new UserNotFoundException("user.role.role-not-existed"));
         user.setRoles(Collections.singleton(role));
 
         // save user entity to db
