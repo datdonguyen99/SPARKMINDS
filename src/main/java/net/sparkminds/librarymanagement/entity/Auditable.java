c75e0dc26eebe2529394ff79ad35be11ae36bf39
DAT-DO <datdonguyen99@gmail.com>
2024-03-05 14:24:31 +0700

    fix auditable

--- src/main/java/net/sparkminds/librarymanagement/entity/Auditable.java
+++ src/main/java/net/sparkminds/librarymanagement/entity/Auditable.java
@@ -3,25 +3,20 @@ package net.sparkminds.librarymanagement.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import net.sparkminds.librarymanagement.config.audit.AuditingEntityListenerImpl;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Optional;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -29,8 +24,9 @@ import java.util.Optional;
 @Setter
 @SuperBuilder
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListenerImpl.class)
 public abstract class Auditable implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @CreatedBy
@@ -42,26 +38,10 @@ public abstract class Auditable implements Serializable {
     private Instant createdDate;
 
     @LastModifiedBy
-    @Column(name = "last_modified_by")
+    @Column(name = "last_modified_by", insertable = false)
     private String lastModifiedBy;
 
     @LastModifiedDate
-    @Column(name = "last_modified_date")
+    @Column(name = "last_modified_date", insertable = false)
     private Instant lastModifiedDate;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdBy = getCurrentAuditor().orElse("unknown");
-        this.createdDate = LocalDateTime.now().toInstant(ZoneOffset.UTC);
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.lastModifiedBy = getCurrentAuditor().orElse("unknown");
-        this.lastModifiedDate = LocalDateTime.now().toInstant(ZoneOffset.UTC);
-    }
-
-    public Optional<String> getCurrentAuditor() {
-        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
-    }
 }
